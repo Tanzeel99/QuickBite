3cfa9b4a444b0ca48504cf8370f6badd8d89d370
Tanzeel99 <mtanzeelkhan99@gmail.com>
2025-06-25 23:23:22 +0530

    removed connection string

--- QuickBite.MessageBus/MessageBus.cs
+++ QuickBite.MessageBus/MessageBus.cs
@@ -10,7 +10,7 @@ namespace QuickBite.MessageBus
 {
     public class MessageBus: IMessageBus
     {
-        private string connectionString = "Endpoint=sb://mangoweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HjoslS58pPHtAULb0tay/jx4Ys0+MO5/R+ASbCcFTG0=";
+        private string connectionString = "";
 
         public async Task PublishMessage(object message, string topic_queue_Name)
         {
